a2fad5bd25aa921609d4640b85400fcaf77d6039
Mari Batilando <mari.batilando@gmail.com>
2014-12-22 18:18:58 -0800

    Add base functionality

--- TipMeMari/ViewController.swift
+++ TipMeMari/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        balanceAmount.text = String(0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,6 +27,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sliderChanged(sender: UISlider) {
+        var currentTipValue = Int(sender.value),
+            total = balanceAmount.text?.floatValue ?? 0.0,
+            newTotal = calculateTotal(total, tip: currentTipValue)
 
+        tipAmount.text = "\(currentTipValue)"
+        totalAmount.text = "\(newTotal)"
+    }
+    
+    func calculateTotal (amount:Int, tip:Int) -> Float {
+        return (amount * Float(tip/100))
+    }
 }
+
+
 
