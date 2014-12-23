210e29d009ffd99338cde6d4e22f01f3d6b28fec
Mari Batilando <mari.batilando@gmail.com>
2014-12-22 16:59:00 -0800

    Finish adding UI fields

--- TipMeMari/ViewController.swift
+++ TipMeMari/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var balanceAmount: UITextField!
+    @IBOutlet weak var tipAmount: NSLayoutConstraint!
+    @IBOutlet weak var totalAmount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
